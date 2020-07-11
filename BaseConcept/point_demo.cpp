7e3823762fa15d1fc1fa806c44cf6ec4b76295c0
Arnni_mi <zhml8951@163.com>
2020-07-11 23:10:31 +0800

    memory. std::unique_ptr demo

--- BaseConcept/point_demo.cpp
+++ BaseConcept/point_demo.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include <memory>
 #include <string>
+#include <sstream>
 
 //指针相关操作记录
 
@@ -12,13 +13,13 @@ double func01(const int num)
 
 // 函数指针与指针函数的区别 ==>
 double* func02(int) = delete;
-double (*func03)(int); // double* fun(int)  返回指针类型的函数即<指针函数>。 double (*func)(int) 指向函数的指针即<函数指针>
+double(*func03)(int); // double* fun(int)  返回指针类型的函数即<指针函数>。 double (*func)(int) 指向函数的指针即<函数指针>
 
 void test_function_pointer()
 {
 	// 定义函数指针，参数为int, 返回值double.
 	// ReSharper disable CppJoinDeclarationAndAssignment
-	double (*p_func01)(int);
+	double(*p_func01)(int);
 	p_func01 = func01;
 
 	printf_s("p_func01 output: %.2lf\n", (*p_func01)(8));
@@ -31,7 +32,7 @@ void test_function_pointer()
 }
 
 namespace csdn_demo01
-// 函数指针直接使用是看不出本来用途的，只有当定义函数指针作形参，传递函数作实参时可实现回调效果，感觉比js回调还要自然。这才是函数指针的直正用途。
+	// 函数指针直接使用是看不出本来用途的，只有当定义函数指针作形参，传递函数作实参时可实现回调效果，感觉比js回调还要自然。这才是函数指针的直正用途。
 {
 	// Begin demo01
 
@@ -47,7 +48,7 @@ namespace csdn_demo01
 		return 0.5 * lines;
 	}
 
-	void estimate(const int line_num, double (*pf)(int))
+	void estimate(const int line_num, double(*pf)(int))
 	{
 		auto ret = (*pf)(line_num);
 		std::cout << "num:  " << line_num << ",  need times is: " << ret << std::endl;
@@ -68,7 +69,7 @@ namespace csdn_demo01
 	// Begin Demo02
 
 	const double* func01(const double arr[], int n);
-	const double* func02(const double [], int);
+	const double* func02(const double[], int);
 	const double* func03(const double*, int);
 
 	void demo02_test()
@@ -84,14 +85,14 @@ namespace csdn_demo01
 		const Pf func03_p = func03;
 		// ReSharper restore CppUseAuto
 
-		double arr[5]{11.1, 12.2, 13.3, 14.4, 15.5};
+		double arr[5]{ 11.1, 12.2, 13.3, 14.4, 15.5 };
 
 		std::cout << "func01_point:  " << (*func01_p)(arr, 3) << "\n";
 		std::cout << "func02_point:  " << func02_p(arr, 3) << "\n";
 		std::cout << "*(func03_point):  " << *((*func03_p)(arr, 3)) << "\n";
 
-		const double* (*func_array_p1[3])(const double*, int){func01, func02, func03};
-		Pf func_array_p2[]{func01, func02, func03};
+		const double* (*func_array_p1[3])(const double*, int) { func01, func02, func03 };
+		Pf func_array_p2[]{ func01, func02, func03 };
 
 		std::cout << "func_p1[0]:  " << func_array_p1[0](arr, 2) << "\n";
 		std::cout << "*(func_p2[0]):  " << *(func_array_p2[0](arr, 2)) << "\n";
@@ -123,8 +124,8 @@ namespace intelligent_point
 {
 	// C++ 提供4种智能指针： auto_ptr, unique_ptr, shared_ptr, weak_ptr;(auto_ptr目前已被弃用)
 
-	// unique_ptr 
-	template <class T, class Deleter=std::default_delete<T>>
+	// unique_ptr
+	template <class T, class Deleter = std::default_delete<T>>
 	class UniquePtr;
 
 	template <typename T, typename Deleter>
@@ -134,7 +135,7 @@ namespace intelligent_point
 	 * unique_ptr 独立拥有它所指的对象， 在某一时刻只能有一个unique_ptr指向特定对象， 当unique_ptr被销毁(reset)时，
 	 * 它所指的对象会自动被销毁。 unique_ptr不允许拷贝构造和赋值操作。只能进行std::move. 也就是右值引用
 	 * unique_ptr release()时放弃它所指对象的控制权，将其置空，并返回对象指针，但并不会释放内存。
-	 * unique_ptr reset(...) 释放ptr对象，并重置ptr的值。 
+	 * unique_ptr reset(...) 释放ptr对象，并重置ptr的值。
 	 */
 
 	void UniquePtrTest()
@@ -161,7 +162,7 @@ namespace intelligent_point
 		str_ptr01.release(); // unique_ptr.release 只是释放控制权， 并不销毁对象
 		printf("str: %s, address: %p\n", str01->c_str(), str01);
 
-		//printf("str: %s,  address: %p\n", str_ptr01->c_str(), &(*str_ptr01)); //unique_ptr失去对象控制， 
+		//printf("str: %s,  address: %p\n", str_ptr01->c_str(), &(*str_ptr01)); //unique_ptr失去对象控制，
 		str_ptr01.reset(nullptr);
 
 		delete db;
@@ -182,11 +183,11 @@ namespace intelligent_point
 	class ClassA
 	{
 	public:
-		ClassA(const std::string& name_cs, const std::string& own_name_cs, const int n_val):
+		ClassA(const std::string& name_cs, const std::string& own_name_cs, const int n_val) :
 			s_name_(name_cs), s_own_name_(own_name_cs)
 		{
 			if (0 == n_val) {
-				std::runtime_error o_rt_ex("nValue can not 0\n");
+				std::runtime_error o_rt_ex("n_val can not 0\n");
 				throw o_rt_ex;
 			}
 			else {
@@ -219,17 +220,42 @@ namespace intelligent_point
 	class ClassB
 	{
 	public:
-		ClassB(int val1, int val2, const std::string& name)
+		ClassB(const int val1, const int val2, const std::string& name)
 		{
 			printf("Name: %s ClassB object begin Construct. \n", name.c_str());
 			a_ptr1_ = new ClassA("a_ptr1", name, val1);
 			a_ptr2_ = new ClassA("a_ptr2", name, val2);
 			this->s_name_ = name;
 		}
+
 		ClassB(const ClassB& be_copy)
 		{
-			//TODO some constructor....
-			// https://blog.csdn.net/xuyouqiang1987/article/details/104127669?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-3.nonecase&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-3.nonecase#default_delete%3C%3E%E7%B1%BB
+			printf_s("ClassB object name: %s Copy constructor begin construct. \n", s_name_.c_str());
+			this->a_ptr1_ = new ClassA(*(be_copy.a_ptr1_));
+			this->a_ptr1_->set_owner_name("ptr1 Copy Constructor.\n");
+			std::ostringstream oss;
+			oss << "Class name " << this->s_name_ << "ClassB object copy constructor occur error. \n";
+			std::runtime_error runtime_error(oss.str());
+			throw runtime_error;
+
+			this->a_ptr2_ = new ClassA(*(be_copy.a_ptr2_));
+			this->a_ptr2_->set_owner_name("ptr2 copy constructor. \n");
+			printf_s("Name: %s  ClassB object finished Copy Constructor. \n", this->s_name_.c_str());
+		}
+
+		const ClassB& operator=(const ClassB& assign_b) const
+		{
+			*(this->a_ptr1_) = *assign_b.a_ptr1_;
+			*(this->a_ptr2_) = *assign_b.a_ptr2_;
+			return *this;
+		}
+
+		~ClassB()
+		{
+			printf_s("Name: %s ClassB object begin Destroy.\n", this->s_name_.c_str());
+			delete this->a_ptr1_;
+			delete this->a_ptr2_;
+			printf_s("Name: %s, ClassB object end Destroy. \n", this->s_name_.c_str());
 		}
 
 	private:
@@ -237,17 +263,70 @@ namespace intelligent_point
 		ClassA* a_ptr2_;
 		std::string s_name_;
 	};
+
+	class ClassUseUnique
+	{
+	public:
+		ClassUseUnique(const int obj1, const int obj2, const std::string& name) : obj_name_(name)
+		{
+			printf_s("Name: %s ClassUseUnique object begin construct. \n", name.c_str());
+			obj_ptr1_.reset(new ClassA("obj1_ptr", obj_name_, obj1));
+			obj_ptr2_ = std::make_unique<ClassA>(ClassA("obj2_ptr", obj_name_, obj2));
+
+			std::unique_ptr<ClassA> obj3_ptr;
+			obj3_ptr = std::unique_ptr<ClassA>(std::move(new ClassA("obj3_ptr", obj_name_, obj2)));
+
+			std::unique_ptr<ClassA> obj4_ptr;
+			printf_s("Name: %s ClassUseUnique object begin construct. \n", obj_name_.c_str());
+		}
+
+		ClassUseUnique(const ClassUseUnique& copy_obj)
+		{
+			printf_s("Name: %s ClassUseUnique object begin copy construct.\n", this->obj_name_.c_str());
+
+			// TODO  add copy constructor. 
+			// https://blog.csdn.net/xuyouqiang1987/article/details/104127669?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-3.nonecase&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-3.nonecase#default_delete%3C%3E%E7%B1%BB
+		}
+
+	private:
+		std::unique_ptr<ClassA> obj_ptr1_;
+		std::unique_ptr<ClassA> obj_ptr2_;
+		std::string obj_name_;
+	};
+
+	void test_cls_ab()
+	{
+		try {
+			ClassB b1(1, 0, "ClassB_b1");
+		}
+		catch (const std::exception& ex) {
+			printf("ClassB b1(1,0) execute occur error... %s\n", ex.what());
+		}
+
+		printf("\n----------------------\n");
+
+		try {
+			const ClassB b101(1, 2, "ClassB_b101");
+			auto b102(b101);
+		}
+		catch (const std::exception& exception) {
+			printf("ClassB b102 execute occur error... %s\n", exception.what());
+		}
+	}
 }
 
 int main(int argc, char* argv[])
 {
-	test_function_pointer();
-	csdn_demo01::test();
-	csdn_demo01::demo02_test();
+	//test_function_pointer();
+	//csdn_demo01::test();
+	//csdn_demo01::demo02_test();
+	//printf("\n----------------------\n");
+	//intelligent_point::UniquePtrTest();
+	//printf("\n----------------------\n");
+	//const auto str = intelligent_point::UniquePtrString();
+	//printf("str: %s, address: %p", str->c_str(), str);
+	//delete str;
+
 	printf("\n----------------------\n");
-	intelligent_point::UniquePtrTest();
-	printf("\n----------------------\n");
-	const auto str = intelligent_point::UniquePtrString();
-	printf("str: %s, address: %p", str->c_str(), str);
-	delete str;
+	intelligent_point::test_cls_ab();
 }
