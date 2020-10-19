e06655e18d7deea342f8df9e3bdd0eddf25ad01d
arnni <zhml8951@126.com>
2020-10-19 16:24:58 +0800

    Win32App Demo01初级

---
+++ Win32App01/Win32App01.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "resource.h"
+
+
+/*
+ *	Windows 开发中句柄使用非常频繁,常用句柄：
+ *		HINSTANCE	=> 实例句柄
+ *		HWND		=> 容器句柄
+ *		HDC			=> 设备句柄
+ *		HICON		=> 图标句柄
+ *		HCURSOR		=> 指针句柄
+ *		HBRUSH		=> 画刷句柄
+ *
+ */
